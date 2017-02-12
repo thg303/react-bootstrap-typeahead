4e1f18344adc57ceeb31ace2ff3068d6b282c089
ali.q <ghanavatian.ali@gmail.com>
2017-02-12 17:29:51 +0330

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-/* eslint-disable object-curly-spacing */
 
-// Components
-export AsyncTypeahead from './AsyncTypeahead';
-export Menu from './Menu';
-export MenuItem from './MenuItem';
-export Token from './Token';
-export Typeahead from './Typeahead';
+export {AsyncTypeahead} from './AsyncTypeahead';
+export {Menu} from './Menu';
+export {MenuItem} from './MenuItem';
+export {Token} from './Token';
+export {Typeahead} from './Typeahead';
 
-// HOCs
-export asyncContainer from './containers/asyncContainer';
-export menuItemContainer from './containers/menuItemContainer';
-export tokenContainer from './containers/tokenContainer';
-
-/* eslint-enable object-curly-spacing */
+export {asyncContainer} from './containers/asyncContainer';
+export {menuItemContainer} from './containers/menuItemContainer';
+export {tokenContainer} from './containers/tokenContainer';
